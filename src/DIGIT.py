9afcf27b57cc83d8d80e567a4a34a4ae68469217
Roberto Canale <roberto.canale.work@gmail.com>
2022-11-08 11:55:50 +0800

    package completion

--- src/DIGIT.py
+++ src/DIGIT.py
@@ -1,5 +1,5 @@
 from digit_interface import Digit
-import cv2, rospy, time
+import cv2, rospy, time, sys
 import numpy as np
 from utils.contact_area_functions import *
 from utils.sensor_functions import *
@@ -8,19 +8,20 @@ from std_msgs.msg import Header
 from cv_bridge import CvBridge
 from digit_ros.msg import contact_center
 
-SENSOR_SERIAL_NUMBER_LIST = ["D20492"]
-SENSOR_NAME_LIST = ["LeftGripper"]
-FPS_CONFIG = Digit.STREAMS["QVGA"]["fps"]["60fps"]
-DIGIT_INTENSITY = 10
 
 class DIGIT:
     def __init__ (self, SERIAL_NUMBER, SENSOR_NAME, FPS_CONFIG = Digit.STREAMS["QVGA"]["fps"]["60fps"], intensity:int = 10):
         self.sn = SERIAL_NUMBER         
         self.name = SENSOR_NAME         
-        self.object =  Digit(SERIAL_NUMBER, SENSOR_NAME)     
-        self.object.connect()
-        self.object.set_fps(FPS_CONFIG)
-        self.object.set_intensity(intensity)        
+        self.object =  Digit(SERIAL_NUMBER, SENSOR_NAME)  
+        try:
+            self.object.connect()
+            self.object.set_fps(FPS_CONFIG)
+            self.object.set_intensity(intensity)   
+        except:
+            print("DIGIT " + SERIAL_NUMBER + " is not connected!")  
+            sys.exit(1)
+   
         self.current_frame_bgr = []
         self.current_frame_lab = []
         self.diff_bgr = []
@@ -41,44 +42,47 @@ class DIGIT:
         #Setting all the publishers
         self.DIGIT_PUBLISHERS()
         
-    def DIGIT_PUBLISHERS (self,rgb_img:str = "rgb", 
-                         lab_img:str = "lab", 
-                         diff_rgb_img:str = "diff_rgb",
-                         diff_lab_img:str = "diff_lab", 
-                         output_img:str = "diff_lab", 
-                         contact_center:str = "contact_center",                         
+    def DIGIT_Publisher (self,rgb_img_topic:str = "rgb", 
+                         lab_img_topic:str = "lab", 
+                         diff_rgb_img_topic:str = "diff_rgb",
+                         diff_lab_img_topic:str = "diff_lab", 
+                         output_img_topic:str = "diff_lab", 
+                         contact_center_topic:str = "contact_center",                         
                          queue_size:int = 10):    
 
-        self.rgb_publisher = rospy.Publisher("/" + self.name + "/" + rgb_img, Image, queue_size=queue_size)
-        self.lab_publisher = rospy.Publisher("/" + self.name + "/" + lab_img, Image, queue_size=queue_size)
-        self.diff_rgb_publisher = rospy.Publisher("/" + self.name + "/" + diff_rgb_img, Image, queue_size=queue_size)
-        self.diff_lab_publisher = rospy.Publisher("/" + self.name + "/" + diff_lab_img, Image, queue_size=queue_size)
-        self.output_publisher = rospy.Publisher("/" + self.name + "/" + output_img, Image, queue_size=queue_size)
-        self.contact_center_publisher = rospy.Publisher("/" + self.name + "/" + contact_center, contact_center, queue_size=queue_size)
+        self.rgb_publisher = rospy.Publisher("/" + self.name + "/" + rgb_img_topic, Image, queue_size=queue_size)
+        self.lab_publisher = rospy.Publisher("/" + self.name + "/" + lab_img_topic, Image, queue_size=queue_size)
+        self.diff_rgb_publisher = rospy.Publisher("/" + self.name + "/" + diff_rgb_img_topic, Image, queue_size=queue_size)
+        self.diff_lab_publisher = rospy.Publisher("/" + self.name + "/" + diff_lab_img_topic, Image, queue_size=queue_size)
+        self.output_publisher = rospy.Publisher("/" + self.name + "/" + output_img_topic, Image, queue_size=queue_size)
+        self.contact_center_publisher = rospy.Publisher("/" + self.name + "/" + contact_center_topic, contact_center, queue_size=queue_size)
         
     def run(self):
-        while True:
-            self.current_frame_bgr = cv2.GaussianBlur(self.object.get_frame(),(11,11),5)
-            self.current_frame_lab = cv2.cvtColor( self.current_frame_bgr, cv2.COLOR_BGR2LAB) 
+        self.current_frame_bgr = cv2.GaussianBlur(self.object.get_frame(),(11,11),5)
+        self.current_frame_lab = cv2.cvtColor( self.current_frame_bgr, cv2.COLOR_BGR2LAB) 
 
-            base_B,base_G,base_R = cv2.split(self.baseline_bgr)
-            base_l,base_a,base_b = cv2.split(self.baseline_lab)
-            
-            curr_B,curr_G,curr_R = cv2.split(self.current_frame_bgr)
-            curr_l,curr_a,curr_b = cv2.split(self.current_frame_lab)
-            
-            self.diff_lab, res_lab, output_img_lab = compute_diff(curr_b, base_b)
-            self.diff_bgr, res_bgr, output_img_bgr = compute_diff(curr_B, base_B)
+        base_B,base_G,base_R = cv2.split(self.baseline_bgr)
+        base_l,base_a,base_b = cv2.split(self.baseline_lab)
+        
+        curr_B,curr_G,curr_R = cv2.split(self.current_frame_bgr)
+        curr_l,curr_a,curr_b = cv2.split(self.current_frame_lab)
+        
+        self.diff_lab, res_lab, output_img_lab = compute_diff(curr_b, base_b)
+        self.diff_bgr, res_bgr, output_img_bgr = compute_diff(curr_B, base_B)
 
-            poly, (major_axis, major_axis_end), (minor_axis, minor_axis_end), center = res_lab
-            self.contact_center.header = Header()
-            self.contact_center.header.stamp = rospy.Time.now()
-            self.contact_
+        poly, (major_axis, major_axis_end), (minor_axis, minor_axis_end), center = res_lab
+        self.contact_center.header = Header()
+        self.contact_center.header.stamp = rospy.Time.now()
+        self.contact_center.center = center
+        self.contact_center.major_axis = major_axis
+        self.contact_center.major_axis_end = major_axis_end
+        self.contact_center.minor_axis = minor_axis
+        self.contact_center.minor_axis_end = minor_axis_end
 
-            
-            self.rgb_publisher.publish(self.bridge.cv2_to_imgmsg(self.current_frame_bgr))
-            self.lab_publisher.publish(self.bridge.cv2_to_imgmsg(self.current_frame_lab))
-            self.diff_rgb_publisher.publish(self.bridge.cv2_to_imgmsg(self.diff_bgr))
-            self.diff_lab_publisher.publish(self.bridge.cv2_to_imgmsg(self.diff_lab))
-            self.output_publisher.publish(self.bridge.cv2_to_imgmsg(output_img_lab))
-            self.contact_center_publisher.publish()
+        
+        self.rgb_publisher.publish(self.bridge.cv2_to_imgmsg(self.current_frame_bgr))
+        self.lab_publisher.publish(self.bridge.cv2_to_imgmsg(self.current_frame_lab))
+        self.diff_rgb_publisher.publish(self.bridge.cv2_to_imgmsg(self.diff_bgr))
+        self.diff_lab_publisher.publish(self.bridge.cv2_to_imgmsg(self.diff_lab))
+        self.output_publisher.publish(self.bridge.cv2_to_imgmsg(output_img_lab))
+        self.contact_center_publisher.publish(self.contact_center)
--- src/multiple_sensors.py
+++ src/multiple_sensors.py
@@ -1,68 +1,41 @@
 
-from digit_interface import Digit
-import cv2
+import rospy
 import numpy as np
 from utils.contact_area_functions import *
 from utils.sensor_functions import *
+from DIGIT import DIGIT
 
-SENSOR_SERIAL_NUMBER_LIST = ["D20492"]
-SENSOR_NAME_LIST = ["LeftGripper"]
-FPS_CONFIG = Digit.STREAMS["QVGA"]["fps"]["60fps"]
-DIGIT_INTENSITY = 10
 
 
 def main():
-    sensors = {}
-    sensors = initialize_sensors(sensors, SENSOR_SERIAL_NUMBER_LIST, SENSOR_NAME_LIST)
-            
-    # compiling set of images into a baseline
-    for _, sensor in sensors.items():
-        sensor = compute_baseline(sensor)
-            
+    ns = rospy.get_name() + "/"
 
-    # digit_pt = pt.PyTouch(pt.sensors.DigitSensor, tasks=[pt.tasks.ContactArea])
+    sn1 = rospy.get_param(ns + "sn1")
+    name1 = rospy.get_param(ns + "name1")
+    rgb_img1 = rospy.get_param(ns + "rgb_img1")
+    lab_img1 = rospy.get_param(ns + "lab_img1")
+    diff_rgb_img1 = rospy.get_param(ns + "diff_rgb_img1")
+    diff_lab_img1 = rospy.get_param(ns + "diff_lab_img1")
+    output_img1 = rospy.get_param(ns + "output_img1")
     
-    while True:
-        output_images_list = []
-        for _, sensor in sensors.items():
-            sensor["current_frame"] = cv2.GaussianBlur(sensor["object"].get_frame(),(11,11),5)
-            
-            baseline = cv2.cvtColor(sensor["baseline"], cv2.COLOR_BGR2LAB)
-            base_l,base_a,base_b = cv2.split(baseline)
-            
-            current_frame = cv2.cvtColor(sensor["current_frame"], cv2.COLOR_BGR2LAB)
-            curr_l,curr_a,curr_b = cv2.split(current_frame)
-            
-            image_diff_norm = abs(base_b - curr_b)**2.
-            image_diff_norm = (image_diff_norm - image_diff_norm.min()) / (image_diff_norm.max() - image_diff_norm.min()) * 255.
-            image_diff_norm = image_diff_norm.astype(np.uint8)
-            
-            raw_stream = contact_area(target=curr_b.copy(),base=base_b)
-            diff, res = raw_stream
-            if not res is None:
-                poly, (major_axis, major_axis_end), (minor_axis, minor_axis_end), center = res
-                print("Contact Center", center)
+    sn2 = rospy.get_param(ns + "sn2")
+    name2 = rospy.get_param(ns + "name2")
+    rgb_img2 = rospy.get_param(ns + "rgb_img2")
+    lab_img2 = rospy.get_param(ns + "lab_img2")
+    diff_rgb_img2 = rospy.get_param(ns + "diff_rgb_img2")
+    diff_lab_img2 = rospy.get_param(ns + "diff_lab_img2")
+    output_img2 = rospy.get_param(ns + "output_img2")
+    
+    rospy.init_node("digit_ros_" + sn2)
+    digit1 = DIGIT(sn1, name1)
+    digit1.DIGIT_Publisher(rgb_img1, lab_img1, diff_rgb_img1, diff_lab_img1, output_img1)
+    digit1.run()
+    
+    digit2 = DIGIT(sn2, name2)
+    digit2.DIGIT_Publisher(rgb_img2, lab_img2, diff_rgb_img2, diff_lab_img2, output_img2)
+    digit2.run()
+    rospy.spin()
 
-                output = draw_major_minor(cv2.cvtColor(curr_b.copy(), cv2.COLOR_GRAY2BGR), poly, major_axis, major_axis_end, minor_axis, minor_axis_end)
-            else:
-                output = curr_b
-                
-                output = cv2.cvtColor(output, cv2.COLOR_GRAY2BGR)
-           
-            diff = (diff * 8. * 255)
-            diff = np.clip(diff, 0., 255.)
-            diff = diff.astype(np.uint8)
-            
-            final_visualized_image = np.hstack((base_b, curr_b, diff))
-            final_visualized_image = cv2.cvtColor(final_visualized_image, cv2.COLOR_GRAY2BGR)
-            final_visualized_image = np.hstack((final_visualized_image, output))
-                       
-        # final_visualized_image = np.vstack(output_images_list)
-        cv2.imshow("Output", final_visualized_image)
-        if cv2.waitKey(1) == 27:
-            for sensor in sensors:
-                sensor.disconnect()
-            break
         
 if __name__ == '__main__':
     main()
